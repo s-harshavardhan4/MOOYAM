1dbbb3f2068e4bd4d55e4c5800c90a5e8a136a96
S. Harsha Vardhan <harshavardhanhm104@gmail.com>
2026-05-26 22:23:08 +0530

    Update checkout.js

--- backend/routes/checkout.js
+++ backend/routes/checkout.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { stripe } from '../../lib/stripe.js';
+import Stripe from 'stripe';
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const router = express.Router();
 
 // POST /api/checkout
