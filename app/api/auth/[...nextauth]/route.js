771418d23ad88edd9c1c55e01de53888628e8e40
S. Harsha Vardhan <harshavardhanhm104@gmail.com>
2026-05-27 06:43:05 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
-export const dynamic = "force-dynamic";
 
 const handler = NextAuth(authOptions);
 
